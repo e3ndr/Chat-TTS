1ac87465463ec1cab3a883d8d5d3a31cc791295f
Ender <33337309+e3ndr@users.noreply.github.com>
2020-09-27 23:56:59 -0500

    Add files via upload

---
+++ module.js
@@ -0,0 +1,55 @@
+
+
+MODULES.moduleClasses["chat_tts"] = class {
+
+    constructor(id) {
+        this.namespace = "chat_tts";
+        this.type = "settings";
+        this.id = id;
+        this.lastclip = 0;
+        this.queue = [];
+
+        const instance = this;
+
+        this.defaultSettings.skip = () => {
+            if (instance.audio) {
+                instance.audio.pause();
+                instance.check(true);
+            }
+        };
+
+        koi.addEventListener("chat", (event) => {
+            this.queue.push(encodeURIComponent(event.message.trim()));
+            this.check();
+        });
+    }
+
+    check(force = false) {
+        if ((force || !this.audio || this.audio.paused) && (this.queue.length > 0)) {
+            this.audio = new Audio("https://api.casterlabs.co/v1/polly?voice=" + this.settings.text_to_speech_voice + "&text=" + this.queue.shift());
+
+            this.audio.addEventListener("end", () => {
+                this.check();
+            });
+
+            this.audio.play();
+        }
+    }
+
+    getDataToStore() {
+        return {
+            text_to_speech_voice: this.settings.text_to_speech_voice
+        };
+    }
+
+    settingsDisplay = {
+        text_to_speech_voice: "select",
+        skip: "button"
+    };
+
+    defaultSettings = {
+        text_to_speech_voice: ["Brian", "Russell", "Nicole", "Amy", "Salli", "Joanna", "Matthew", "Ivy", "Joey"],
+        // skip: () => {}
+    };
+
+};
